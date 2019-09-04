fb1b9fabc668bccda243e9185f412456f4dbbe8d
Anthony <vuonga91@gmail.com>
2019-09-04 11:26:54 -0700

    Update ingress to specify which CIDR blocks allow access

--- main.tf
+++ main.tf
@@ -7,9 +7,10 @@ resource "aws_security_group" "sg_disable_group" {
   dynamic ingress {
     for_each = var.ingress_ports
     content {
-      from_port = ingress.value
-      to_port   = ingress.value
-      protocol  = "tcp"
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = "${var.ingress_allowed}"
     }
   }
 
