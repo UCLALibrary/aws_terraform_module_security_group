ba66f076a567949921047e95a460279609a1879d
Anthony Vuong <avuong@cachemeoutside.io>
2019-09-04 22:56:42 -0700

    simplify setup to use null vars

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
-resource "aws_security_group" "sg_disable_group" {
-  count         = "${var.disable_sg_group}"
+resource "aws_security_group" "sg" {
   name          = "${var.sg_name}"
   description   = "${var.sg_description}"
   vpc_id        = "${var.vpc_id}"
@@ -7,34 +6,11 @@ resource "aws_security_group" "sg_disable_group" {
   dynamic ingress {
     for_each = var.ingress_ports
     content {
-      from_port   = ingress.value
-      to_port     = ingress.value
-      protocol    = "tcp"
-      cidr_blocks = "${var.ingress_allowed}"
-    }
-  }
-
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group" "sg_enable_group" {
-  count         = "${var.enable_sg_group}"
-  name          = "${var.sg_name}"
-  description   = "${var.sg_description}"
-  vpc_id        = "${var.vpc_id}"
-
-  dynamic ingress {
-    for_each = var.ingress_ports
-    content {
-      from_port = ingress.value
-      to_port   = ingress.value
-      protocol  = "tcp"
-    security_groups = "${var.sg_groups}"
+      from_port       = ingress.value
+      to_port         = ingress.value
+      protocol        = "tcp"
+      cidr_blocks     = "${var.ingress_allowed}"
+      security_groups = "${var.sg_groups}"
     }
   }
 
--- variables.tf
+++ variables.tf
@@ -3,7 +3,5 @@ variable "sg_description" {}
 variable "vpc_id" {}
 variable "ingress_ports" {}
 variable "ingress_allowed" { default = ["0.0.0.0/0"] }
-variable "sg_groups" {}
-variable "enable_sg_group" { default = 0 }
-variable "disable_sg_group" { default = 0 }
+variable "sg_groups" { default = null }
 
