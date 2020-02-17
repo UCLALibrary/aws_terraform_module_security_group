df040d537c3afb07b74d843e004be5c292a0dcda
Anthony Vuong <avuong@cachemeoutside.io>
2020-02-16 20:27:07 -0800

    Fix variable formatting

--- variables.tf
+++ variables.tf
@@ -3,8 +3,9 @@ variable "sg_description" {}
 variable "vpc_id" {}
 variable "ingress_ports" {}
 variable "ingress_allowed" {
-    default = ["0.0.0.0/0"]
+  default = ["0.0.0.0/0"]
 }
+
 variable "sg_groups" {
-    default = null
+  default = null
 }
